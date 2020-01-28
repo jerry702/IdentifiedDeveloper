008db06a760358537b4591db8780f9894b7266ba
jerry702 <55716043+jerry702@users.noreply.github.com>
2020-01-28 08:27:32 -0500

    Update IdentifiedDeveloper.py
    Julieann11202@icloud.com

--- IdentifiedDeveloper.py
+++ IdentifiedDeveloper.py
@@ -1,5 +1,6 @@
 import os
 
-path_name = input("Please enter the path of the file you want to unblock: ")
+path_name = input("julieann11202@icloud.com
+Please enter the path of the file you want to unblock: ")
 
 os.system("xattr -d com.apple.quarantine \"" + str(path_name) + "\"")
